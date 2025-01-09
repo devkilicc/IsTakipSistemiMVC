de31c216ed64de530a0e983a0e801233489aa794
Muhammed KILIC <devkilicc@gmail.com>
2025-01-10 00:12:56 +0300

    LoginController added

---
+++ src/Backend/IsTakipSistemiMVC/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using IsTakipSistemiMVC.Models;
+
+namespace IsTakipSistemiMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        IsTakipDBEntities entity = new IsTakipDBEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
