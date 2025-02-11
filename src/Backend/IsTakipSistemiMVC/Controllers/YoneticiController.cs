62095a46453f0c37055d7888af773804dd4d067e
Muhammed KILIC <devkilicc@gmail.com>
2025-02-11 11:18:03 +0300

    Yönetici girişi eklendi.

---
+++ src/Backend/IsTakipSistemiMVC/Controllers/YoneticiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using IsTakipSistemiMVC.Models;
+
+namespace IsTakipSistemiMVC.Controllers
+{
+    public class YoneticiController : Controller
+    {
+        IsTakipDBEntities entity = new IsTakipDBEntities();
+        // GET: Yönetici
+        public ActionResult Index()
+        {
+            int yetkiTurId = Convert.ToInt32(Session["PersonelYetkiTurId"]);
+
+            if (yetkiTurId == 1)
+            {
+                int birimId = Convert.ToInt32(Session["PersonelBirimId"]);
+
+                var birim = (from b in entity.Birimler where b.birimId == birimId select b).FirstOrDefault();
+
+                ViewBag.birimAd = birim.birimAd;
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+    }
+}
